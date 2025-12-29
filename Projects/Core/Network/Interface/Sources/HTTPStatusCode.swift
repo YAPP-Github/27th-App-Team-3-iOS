be66e71eeeaa1eb25d4e6db6d52377f4276b945e
jihun <jihun332@gmai.com>
2025-12-29 21:33:00 +0900

    chore: magic number 경고 처리를 위해 HTTPStatusCode 분리 - #28

--- Projects/Core/Network/Interface/Sources/HTTPStatusCode.swift
+++ Projects/Core/Network/Interface/Sources/HTTPStatusCode.swift
@@ -1,5 +1,5 @@
 //
-//  StatusCode.swift
+//  HTTPStatusCode.swift
 //  CoreNetworkInterface
 //
 //  Created by 정지훈 on 12/29/25.
@@ -7,9 +7,9 @@
 
 import Foundation
 
-enum HTTPStatusCode {
-    static let success = 200...299
-    static let badRequest = 400
-    static let unauthorized = 401
-    static let serverError = 500...599
+public enum HTTPStatusCode {
+    public static let success = 200...299
+    public static let badRequest = 400
+    public static let unauthorized = 401
+    public static let serverError = 500...599
 }
